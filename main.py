53d949dc41af5287e4b5ddd05f268d6bb516a5fe
NEDONION <53420678+NEDONION@users.noreply.github.com>
2021-12-09 19:21:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from PIL import Image
 
 st.set_page_config(
 page_title="Welcome to Borrow Faster!",
-page_icon= ":robot:",
+page_icon= ":fish:",
 layout="wide",
 initial_sidebar_state="expanded",
 menu_items={
